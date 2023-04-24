26ed0e3fc4c333c1c4bd45f72638ff2f2082e496
unknown <SUITE@SUITEBG210-ST06.cibertec.edu.pe>
2023-04-24 14:49:48 -0500

    Inicio del Proyecto

---
+++ src/main/java/com/cibertec/DawiCl1CarolinaApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiCl1CarolinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiCl1CarolinaApplication.class, args);
+	}
+
+}
